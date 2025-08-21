0b6887dbb4e5ee1534175f0e435f2b9323e63f3d
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-08-21 17:22:23 +1000

    Add ProfilePage

---
+++ src/components/ProfilePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SiteBanner from './elements/SiteBanner.vue';
+import NavBar from './elements/NavBar.vue';
+</script>
+
+<template>
+    <SiteBanner />
+    <NavBar />
+</template>
+
+<style>
+
+</style>
