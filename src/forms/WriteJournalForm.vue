e1d106d0df45cac9e7f9ff7f8975550d82885c19
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-08-27 15:12:46 +1000

    Benefit of Journaling

--- src/forms/WriteJournalForm.vue
+++ src/forms/WriteJournalForm.vue
@@ -1,32 +1,78 @@
 <template>
-    <form @submit.prevent="">
+    <form @submit.prevent="writeJournal()">
         <h2>What's on your mind</h2>
-        <span class="your">Your Feeling</span>
-        <label>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-            <input type="checkbox" name="" value=""/>
-        </label>
-        <label for="content" class="form-label">Your story</label>
-        <textarea class="form-control" id="content" row="3" v-model="journalForm.content"/>
+        <span class="category">Your Feeling</span>
+        <div class="feelings" @blur="validateMood(true)" @input="validateMood(false)">
+
+            <input type="checkbox" value="" v-model="journalForm.moods" />
+            <label class="form-check-label" for=""></label>
+               
+        </div>
+
+        <label for="content" class="form-label"><span class="category">Your story</span></label>
+        <textarea class="form-control" id="content" row="3" v-model="journalForm.content" ></textarea>
     </form>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
-const journalForm = ref({
-
+const journalForm = reactive({
+    moods: [],
     content: '',
-})
+    timestamp: Date()
+});
+
+const writeJournal = () => {
+    validateMood(true);
+    validateContent(true);
+}
+
+const errors = {
+    moods: null,
+    content: null
+}
+
+const validateMood = (blur) => {
+    if (journalForm.moods.length == 0) {
+        if (blur) errors.moods = 'Please specify what do you feel.';
+    } else {
+        errors.moods = null;
+    }      
+}
+
+const validateContent = (blur) => {
+    if (journalForm.content.length == 0) {
+        if (blur) errors.content = 'Please write your journal.';
+    } else if (journalForm.content.length > 100) {
+        if (blur) errors.content = 'The journal length is too long. (Maximum = 100 characters)';
+    } else {
+        errors.content = null;
+    } 
+}
 
 </script>
 
-<style>
+<style scoped>
+.feeling {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+}
 
+@media (min-width: 768px) {
+    .feeling {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr); 
+        grid-template-rows: repeat(4, 1fr); 
+    }
+}
+
+@media (min-width: 1200px) {
+    .feeling {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr); 
+        grid-template-rows: repeat(4, 1fr); 
+    }
+}
 </style>
--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -1,9 +1,54 @@
 <script setup>
-
+import WriteJournalForm from '@/forms/WriteJournalForm.vue';
 </script>
 
 <template>
-    <div class="container"></div>
+    <div class="container d-flex flex-row">
+        <div class="col-lg-4 col-12">
+            <h2>5 Benefits of Writing Journal</h2>
+            <blockquote>
+                Journaling offers numerous benefits for mental health, especially
+                stress reduction, emotional regulation and self-awareness.
+                Your journal is your safe space without relying on anyone else.
+            </blockquote>
+            <ol>
+                <li>
+                    <span class="category">Stress Reduction and Emotional Processing</span>
+                    <p>
+                        Journaling helps you understand what you are worrying about.
+                    </p>
+                </li>
+                <li>
+                    <span class="category">Enhanced Self-Awareness and Insight</span>
+                    <p>
+                        The act of writing induces self-reflection and deepen mood understanding.
+                    </p>
+                </li>
+                <li>
+                    <span class="category">Improved Mood and Mental Well-being</span>
+                    <p>
+                        By acknowledging happy and sorrow memories, journaling can help boosting happiness and avoiding sadness.
+                    </p>
+                </li>
+                <li>
+                    <span class="category">Increased Resilience and Coping Skills</span>
+                    <p>
+                        Jotting down past events understand how you deal with each events and how you learn from that incidents.
+                    </p>
+                </li>
+                <li>
+                    <span class="category">Relaxation</span>
+                    <p>
+                        After writing down your worries, you can still read your journals anytime and understand that feelings.
+                    </p>
+                </li>
+            </ol>
+            <cite></cite>
+        </div>
+        <div class="col-lg-8 col-12">
+            <WriteJournalForm />
+        </div>
+    </div>
 </template>
 
 <style>
