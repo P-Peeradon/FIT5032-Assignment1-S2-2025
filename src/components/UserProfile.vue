b13bee72f0ffc10e440fb3b07d3060ad6a51c857
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-08-27 14:08:29 +1000

    Adjust margin on top

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container d-flex flex-column mt-5">
-        <h1>User Profile</h1>
+    <div class="container d-flex flex-column ">
+        <h1 class="mt-2">User Profile</h1>
         <div class="d-flex flex-row gx-5">
             <div class="d-flex flex-column mt-4 col-md-4 col-12">
                 
