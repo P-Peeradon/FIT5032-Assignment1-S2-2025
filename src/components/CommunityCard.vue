4e4ddd4bd9d8cd02471cf07b48a13c6b33350c81
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-08-28 11:31:33 +1000

    Validate club logo.

--- src/components/CommunityCard.vue
+++ src/components/CommunityCard.vue
@@ -1,8 +1,9 @@
 <template>
-    <router-link :to="{ name: 'Club', params: {name: community.name}}" class="nav-link" active-class="active" aria-current="page">
+    <router-link :to="{ name: 'Club', params: {name: props.community.name}}" class="nav-link" active-class="active" aria-current="page">
         <div class="commuCard">
-            <img :src="community.logo" :alt="community.name" />
-            <h2>{{ community.name.toUppercase() }}</h2>
+            <img v-if="logoExist()" :src="props.community.logo" :alt="props.community.name" />
+            <img v-else src="props.community.logo" :alt="props.community.name"/>
+            <h2>{{ props.community.name.toUppercase() }}</h2>
         </div>
     </router-link>
 </template>
@@ -16,6 +17,10 @@ const props = defineProps({
         required: true
     }
 });
+
+const logoExist = () => {
+    return props.community.logo != null && props.community.logo !== '' 
+}
 </script>
 
 <style scoped>
@@ -25,5 +30,8 @@ const props = defineProps({
     box-sizing: border-box;
     box-shadow: 0px 5px 40px rgb(192, 192, 192);
 }
-
+img {
+    width: 250px;
+    height: 250px;
+}
 </style>
--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,12 +1,66 @@
 <template>
     <div class="container d-flex flex-column">
         <h1>Community</h1>
-        <div class="d-flex flex-column flex-lg-row"></div>
+        <div class="d-flex flex-column flex-lg-row">
+            <main class="col-md-9 col-lg-8">
+                <h2>Select your preferred community.</h2>
+            </main>
+            <aside class="col-md-3 col-lg-4"></aside>
+        </div>
     </div>
 </template>
 
 <script setup>
 
+const communities = [
+    {
+        name: 'society A', 
+        abbrev: '', 
+        owner: 'Kaiba Noah', 
+        location: 'Melbourne', 
+        licensedIn: 'Australia', 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ornare dolor, vitae interdum eros. Morbi id dolor ut elit lacinia ultrices sed id neque. Nulla gravida nulla nec velit lobortis, ac aliquet quam gravida. Morbi molestie gravida eros non tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam suscipit volutpat pellentesque. Mauris non purus a nulla fermentum convallis. Nunc sed laoreet magna. Etiam commodo leo vitae feugiat cursus. Quisque elit sapien, convallis in justo at, suscipit iaculis augue. Nulla facilisi. Cras aliquam hendrerit mattis. Vivamus vitae pretium turpis, sit amet elementum purus. Morbi lorem ante, viverra at finibus a, ornare nec sem. Suspendisse libero nunc, facilisis quis interdum et, iaculis quis metus.'},
+    {
+        name: 'society B', 
+        abbrev: 'BH', 
+        owner: 'Hakuba Saguru', 
+        location: 'Sydney', 
+        licensedIn: 'Australia', 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ornare dolor, vitae interdum eros. Morbi id dolor ut elit lacinia ultrices sed id neque. Nulla gravida nulla nec velit lobortis, ac aliquet quam gravida. Morbi molestie gravida eros non tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam suscipit volutpat pellentesque. Mauris non purus a nulla fermentum convallis. Nunc sed laoreet magna. Etiam commodo leo vitae feugiat cursus. Quisque elit sapien, convallis in justo at, suscipit iaculis augue. Nulla facilisi. Cras aliquam hendrerit mattis. Vivamus vitae pretium turpis, sit amet elementum purus. Morbi lorem ante, viverra at finibus a, ornare nec sem. Suspendisse libero nunc, facilisis quis interdum et, iaculis quis metus.'
+    },
+    {
+        name: 'society C', 
+        abbrev: 'CHD', 
+        owner: 'Hakuba Saguru', 
+        location: 'Melbourne', 
+        licensedIn: 'Australia', 
+        description: ''
+    },
+    {
+        name: 'society D', 
+        abbrev: '', 
+        owner: 'Akaba Reiji', 
+        location: 'Auckland', 
+        licensedIn: 'New Zealand', 
+        description: ''
+    },
+    {
+        name: 'society E', 
+        abbrev: '', 
+        owner: 'Sambejima Hokutou', 
+        location: 'Adelaide', 
+        licensedIn: 'Australia', 
+        description: ''
+    },
+    {
+        name: 'society F', 
+        abbrev: 'FIT', 
+        owner: 'Kudo Yusaku', 
+        location: 'Singapore', 
+        licensedIn: 'Singapore', 
+        description: ''
+    } 
+];
 </script>
 
 <style>
