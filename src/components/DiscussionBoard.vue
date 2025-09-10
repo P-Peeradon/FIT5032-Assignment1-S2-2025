929f741f01ccd6af520d5a752cfeefe2d5c3634b
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-09-10 18:04:28 +1000

    Discussion board for clubs.

--- src/components/DiscussionBoard.vue
+++ src/components/DiscussionBoard.vue
@@ -24,14 +24,14 @@ const query = ref('');
             <div class="list_post" v-for="post in club.posts" :key="post">
                 <div  @click="displayComment(post)">
                     <h2>{{ post.topic }}</h2>
-                    <p>{{ post.owner }} {{ post.timestamp }}</p>
+                    <p>{{ post.username }} {{ post.timestamp }}</p>
                 </div>
             </div>
         </aside>
         <main class="col-9">
             <div v-if="currentPost">
                 <h2>{{ post.topic }}</h2>
-                <q>{{ post.owner }}</q>
+                <q>{{ post.username }}</q>
                 <p>{{ post.content }}</p>
                 <div v-for="comment in post.comments" :key="comment">
                     <p>{{ comment }}</p>
