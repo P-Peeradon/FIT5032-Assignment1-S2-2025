226d684998191064f1cd6fa0786b57956e100c72
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-09-11 17:51:36 +1000

    VULNERABILITY DETECTED: remove all confidential data from firebase/init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -6,11 +6,11 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: "fit5032-peeradon-assignment1",
+  projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.env.VITE_APP_ID,
-  measurementId: import.meta.env.env.VITE_MEASUREMENT_ID
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
