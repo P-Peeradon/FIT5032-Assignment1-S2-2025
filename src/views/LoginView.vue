250301a1c51072584d886646cc3602241ca68682
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-09-11 17:42:22 +1000

    Remove unused function.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import db from '../firebase/init'
 import LoginForm from '../forms/LoginForm.vue';
